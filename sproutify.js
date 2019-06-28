47d7c9ce2485877c204a0e41f53d9ebc1956a1f5
Christian Stuff <christianstuff90@gmail.com>
2019-06-28 16:55:53 +0200

    add avocados 🥑

--- sproutify.js
+++ sproutify.js
@@ -11,7 +11,7 @@ const sproutImagePaths = [
 ]
 
 const potatoImagePaths = [
-  "images/potatoes/bowl-1842294_1920.jpg",   
+  "images/potatoes/bowl-1842294_1920.jpg",
   "images/potatoes/potatoes-1866415_1920.jpg",
   "images/potatoes/eat-4239509_1920.jpg",
   "images/potatoes/prince-abid-1139407-unsplash.jpg",
@@ -19,17 +19,29 @@ const potatoImagePaths = [
   "images/potatoes/ukraine-2652561_1920.jpg"
 ]
 
+const avocadoImagePaths = [
+  "images/avocados/charles-rnxGtZJl1Q8-unsplash.jpg",
+  "images/avocados/FoodieFactor-avocado-pixabay.jpg",
+  "images/avocados/irene-kredenets-AWMWcR3hQUg-unsplash.jpg",
+  "images/avocados/mali-maeder-avocado-pexels.jpg",
+  "images/avocados/nur-afni-setiyaningrum-NIBKggQ-bVM-unsplash.jpg",
+  "images/avocados/silverstylus-avocado-pixabay.jpg",
+  "images/avocados/thought-catalog-EMX1eJ1BcgU-unsplash.jpg",
+  "images/avocados/wixin_56l-avocado-pixabay.png"
+]
+
 const map = {
   'sprouts': sproutImagePaths,
-  'potatoes': potatoImagePaths
+  'potatoes': potatoImagePaths,
+  'avocados': avocadoImagePaths
 }
 
 function restoreOptionsPromise() {
   // Use default value veggie = 'sprouts'
   const promise = browser.storage.sync.get({
     preferredVeggie: 'sprouts'
-  }); 
-  return promise; 
+  });
+  return promise;
 }
 
 const getRandomSprout = (imagePath) => {
